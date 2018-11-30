b7061830f76627c48dd20c86f681dddc17e92a0a
Eric Seguin <eric@segwin.ca>
2018-11-29 20:22:32 -0500

    Add very basic charts implementation to web page (no data yet--still need to add a service!)

--- web/src/app/monitor/monitor.component.ts
+++ web/src/app/monitor/monitor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Monitor } from './monitor';
+import { Chart } from 'chart.js'
 
 @Component({
   selector: 'app-monitor',
@@ -8,13 +9,119 @@ import { Monitor } from './monitor';
 })
 
 export class MonitorComponent implements OnInit {
-
   constructor() {}
 
-  ngOnInit() {
+  private async delay(ms: number) {
+      await new Promise(resolve => setTimeout(()=>resolve(), ms)).then(()=>console.log("fired"));
   }
 
-  monitors: Monitor[] = [ { id: 0 }, { id: 1 } ];
+  private async initCharts() {
+    // TODO: How can we do this cleanly?
+    while (!document.getElementById('temperature-' + this.monitors[0].id.toString())) {
+      await this.delay(50);
+    }
+
+    this.monitors.forEach((monitor) => {
+      let timeMockData = [ 1542772336, 1543772336, 1544772336, 1545772336, 1546772336 ];
+      let timeMockDataFmt : string[] = [];
+      timeMockData.forEach((res) => { timeMockDataFmt.push((new Date(res * 1000)).toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric' })) });
+
+      let temperatureIntMockData = [ 5.0, 6.0, 3.0, 4.0, 3.0 ];
+      let temperatureExtMockData = [ -6.0, -2.0, 1.0, -4.0, -11.0 ];
+      let humidityIntMockData = [ 40.0, 45.0, 43.0, 52.0, 57.0 ];
+      let humidityExtMockData = [ 20.0, 22.0, 18.0, 27.0, 23.0 ];
+
+      let temperatureId = 'temperature-' + monitor.id.toString();
+      let humidityId = 'humidity-' + monitor.id.toString();
+
+      monitor.temperatureChart = new Chart(temperatureId, {
+        type: 'line',
+
+        data: {
+          labels: timeMockDataFmt,
+          datasets: [
+            {
+              label: 'Interior',
+              data: temperatureIntMockData,
+              borderColor: "#c10767",
+              pointBackgroundColor: "#c10767",
+              pointHitRadius: 6,
+              fill: false
+            },
+            {
+              label: 'Exterior',
+              data: temperatureExtMockData,
+              borderColor: "#0767c1",
+              pointBackgroundColor: "#0767c1",
+              pointHitRadius: 6,
+              fill: false
+            }
+          ]
+        },
+
+        options: {
+          legend: {
+            display: true
+          },
+          scales: {
+            xAxes: [{
+              display: false
+            }],
+            yAxes: [{
+              display: true
+            }],
+          }
+        }
+      });
+
+      monitor.humidityChart = new Chart(humidityId, {
+        type: 'line',
+
+        data: {
+          labels: timeMockDataFmt,
+          datasets: [
+            {
+              label: 'Interior',
+              data: humidityIntMockData,
+              borderColor: "#c10767",
+              pointBackgroundColor: "#c10767",
+              pointHitRadius: 6,
+              fill: false
+            },
+            {
+              label: 'Exterior',
+              data: humidityExtMockData,
+              borderColor: "#0767c1",
+              pointBackgroundColor: "#0767c1",
+              pointHitRadius: 6,
+              fill: false
+            }
+          ]
+        },
+
+        options: {
+          legend: {
+            display: true
+          },
+
+          scales: {
+            xAxes: [{
+              display: false
+            }],
+            yAxes: [{
+              display: true
+            }],
+          },
+        }
+      });
+    });
+  }
+
+  ngOnInit(): void {
+    this.initCharts();
+  }
+
+  monitors: Monitor[] = [ new Monitor(0), new Monitor(1) ];
 
   selectedMonitor: Monitor;
   onSelect(monitor: Monitor): void {
--- web/src/app/monitor/monitor.ts
+++ web/src/app/monitor/monitor.ts
@@ -1,3 +1,17 @@
-export class Monitor {
-    id: number
+import { OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
+export class Monitor implements OnInit {
+    id: number;
+
+    temperatureChart : Chart = null;
+    humidityChart : Chart = null;
+    massChart : Chart = null;
+
+    constructor(id: number) {
+        this.id = id;
+    }
+
+    ngOnInit() : void {
+    }
 }
