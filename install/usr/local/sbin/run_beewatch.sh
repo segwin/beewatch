e32fd2d0ca201ee962eb715f7a9f2d6b58ae0044
Eric Seguin <eric@segwin.ca>
2018-12-24 00:25:39 -0500

    Replace runner script with service (Restart=Always to manage crashes)

--- install/usr/local/sbin/run_beewatch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run beewatch and restart it if a crash occurs
-# Courtesy: https://stackoverflow.com/a/697064
-until beewatch; do
-    echo "Program crashed with exit code $?. Respawning..." >&2
-    sleep 1
-done
